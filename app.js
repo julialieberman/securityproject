fb575884ae8a118297853c0a353556b173326068
julialiebs <jlieberman@scu.edu>
2020-12-01 11:24:43 -0800

    keys

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+global.crypto = require('crypto');
+
 // my scripts
 const decryption = require('./decryption.js');
 var app = express();
 app.use(express.json());
-
+var https = require('https');
+var request = require('request');
 const server= require('http').createServer(app);
 app.use(express.static('./'))
 
@@ -11,10 +14,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
-const encryptedSharedKey="7J9dmsS}xGsf6@-oMDzGwar8r7lv9ZPCo/DYTH0F";
-//const apiSharedSecret="n8+6AvGu/i2qo86FQvZ4TLgsS56jqOSGzgOa@Eul";
-
+const apiKey='XBEAIH512D4IEHUNR3U921VmvlTWenZGBUYa5rpqRpQkF57nY';
+const sharedSecret='7J9dmsS}xGsf6@-oMDzGwar8r7lv9ZPCo/DYTH0F'; //encryption shared secret
+const encryptionAPIKey='S0T43UBS7EVRJ2JW1ZYT13TJWadUp8f43_43Kgr0tgNsjb7rI';
+const apiSharedSecret="n8+6AvGu/i2qo86FQvZ4TLgsS56jqOSGzgOa@Eul";
+var queryParams = 'apikey='+apiKey;
+var fullUnhashedString=queryParams+'&encryptionKey='+encryptionAPIKey;
+var postBody= '';
 // render the html pages. the '/<name>' is what the url extension will look like in the browser
 app.get('/', function (req, res) {
     res.render('index.html')
@@ -23,14 +29,53 @@ app.get('/', function (req, res) {
 app.post('/encrypteddata', function(req, res){
     console.log("trying to decrypt data!");
     console.log(req.body);
-    let encKey=req.body.encKey;
-    let encPayment=req.body.encPaymentData;
+    //encrypted key to be used to decrypt encPaymentData. Used shared secret to decrypt this key
+    var encKey=req.body.encKey;
+    //encrypted consumer and payment data. decrypt by unwrapping encKey, then using that value to decrypt this
+    var encPayment=req.body.encPaymentData;
     console.log(encKey);
     console.log(encPayment);
-    decryption.decryptPayload(encryptedSharedKey, encKey, encPayment);
+    //decryption.decryptPayload(sharedSecret, encKey, encPayment);
 
 
-    res.send("Successfully sent encrypted data to server");
+
+    console.log("Trying out a new thing now");
+    var timestamp = Math.floor(Date.now()/1000);
+    var resourcePath="payment/data/"+req.body.callid;
+    var preHashString = timestamp + resourcePath + fullUnhashedString+postBody;
+    console.log("prehash string is: ");
+    console.log(preHashString);
+    var hashString = crypto.createHmac('sha256', sharedSecret).update(preHashString).digest('hex');
+
+	var xPayToken = 'xv2:'+timestamp + ':' + hashString;
+    console.log(hashString);
+	console.log(xPayToken);
+    let callId=req.body.callid;
+    var options = {
+        uri: 'https://sandbox.secure.checkout.visa.com/wallet-services-web/payment/data/'+callId+hashString,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'x-pay-token': xPayToken
+        },
+        json: true
+    };
+
+        console.log(options);
+
+        options.agent = new https.Agent(options);
+
+        request.get(options, (err, res, body) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(`Status: ${res.statusCode}`);
+            console.log(body);
+        });
+        res.send('Hello World');
+
+   // res.send("Successfully sent encrypted data to server");
 })
 
 /** on page error */
@@ -39,4 +84,4 @@ app.use(function (req, res, next)
     res.status(404).send("404 not found. :(");
 })
 
-server.listen(5000, () => console.log('App listening on http://localhost:5000')); //listen to port 5000
+server.listen(5000, () => console.log('App listening on https://localhost:5000')); //listen to port 5000
--- decryption.js
+++ decryption.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 // key = encryption shared secret
 // wrappedKey = req.body.encKey
-// payload = req.body.encPayloadData or something, from V.on(payment.success)
+// payload = req.body.encPayloadData from V.on(payment.success), encrypted user data
 module.exports = {
     decryptPayload: function(key,wrappedKey,payload){
         let decryptedKey = decrypt(wrappedKey,key);
@@ -19,16 +19,14 @@ function decrypt(encrypted,key){
     let data = Buffer.from(encryptedBuffer).slice(48); //data = first 48 bytes
 
     // now we will decrypt remaining data using AES-256-CBC, the IV from above and the hash of the shared secret
-    var hash = crypto.createHmac('SHA256', key).update
-    (Buffer.concat([iv,data])).digest();
+    var hash = crypto.createHmac('sha256', key).update(Buffer.concat([iv,data])).digest();
     if(!hmac.equals(hash))
     {
         // Handle HMAC validation failure
         console.error("HMAC does not equal hash, decryption.js line 24");
         return ''; 
     }
-    let decipher = crypto.createDecipheriv('aes-256-cbc',
-    crypto.createHash('sha256').update(key).digest(), iv);
+    let decipher = crypto.createDecipheriv('aes-256-cbc', crypto.createHash('sha256').update(key).digest(), iv);
     let decryptedData = Buffer.concat([decipher.update(data), decipher.final()]);
     console.log("decrypted data");
     console.log(decryptedData);
